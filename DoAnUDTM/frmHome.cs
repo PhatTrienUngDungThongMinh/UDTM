9982cd4dd6b700bc9894214548653f42cb5c17b9
tinhbe <98801099+tvantinh@users.noreply.github.com>
2024-10-14 20:39:11 +0700

    update linQ

--- DoAnUDTM/frmHome.cs
+++ DoAnUDTM/frmHome.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using DoAnUDTM.DAL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Linq;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,25 @@ namespace DoAnUDTM
 {
     public partial class frmHome : Form
     {
+       
         public frmHome()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void frmHome_Load(object sender, EventArgs e)
+        {
+            //var products = db.Category.ToList(); // Fetch all products from the database
+
+            //// Assign the products list to the DataGridView's DataSource
+            //dataGridView1.DataSource = products;
+            //dataGridView1 = new DataGridView();
+            //dataGridView1.DataSource = 
         }
     }
 }
